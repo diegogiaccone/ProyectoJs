f2752e1c633dd54e2aa8d0962773da23a2922902
diegoG <diego_fg91@hotmail.com>
2023-01-12 19:30:14 -0300

    -

--- js/eventos.js
+++ js/eventos.js
@@ -1,35 +1,72 @@
-const clickbtn = document.querySelectorAll(".button")
+//variables globales
+const lista = document.querySelector("#productos");
 const tbody = document.querySelector(".tbody")
 let carrito = []
 
-clickbtn.forEach(btn =>{
-    btn.addEventListener("click", addToCarritoItem)
-})
+const asincronismo = async () => {
+  const res = await fetch("/json/data.json")
+  const data = await res.json();
+  data.forEach((producto) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="col d-flex justify-content-center mb-4">
+            <div class="card shadow mb-1 bg-dark rounded" style="width: 20rem">
+              <h5 class="card-title pt-2 text-center text-primary">${producto.nombre}</h5>
+              <img
+                src="${producto.img}"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <p class="card-text text-white-50 description">
+                  Edición limitada no te lo podes perder. Esta increible oferta
+                  expira el 30 de marzo del 2023.
+                </p>
+                <h5 class="text-primary">
+                  Precio: <span class="precio">$ ${producto.precio}</span>
+                </h5>
+                <div class="d-grid gap-2">
+                  <button class="btn btn-primary button" id="${producto.id}">
+                    Añadir al Carrito
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+    `
+    lista.append(div);
+  })
 
+  const clickbtn = document.querySelectorAll(".button")
+  clickbtn.forEach(btn =>{
+      btn.addEventListener("click", addToCarritoItem)  
+  })
+
+}
+
+asincronismo();
+
+
+//agregar productos al carrito
 function addToCarritoItem(e){
     const button = e.target    
     const item = button.closest(".card")    
     const itemTitle = item.querySelector(".card-title").textContent;    
     const itemPrice = item.querySelector(".precio").textContent;   
     const itemImg = item.querySelector(".card-img-top").src;    
+    console.log(itemImg);
     const newItem = {
         title: itemTitle,
         precio: itemPrice,
         img: itemImg,
         cantidad: 1
-    }
-
-    addItemCarrito(newItem)
+    }   
+    addItemCarrito(newItem)  
 }
 
 function addItemCarrito(newItem){
 
-    const alert = document.querySelector(".alert")
-
-    setTimeout(function(){
-        alert.classList.add("hide")
-    }, 2000)
-        alert.classList.remove("hide")
+  modal(newItem.img)
 
     const InputElemento = tbody.getElementsByClassName("input__elemento")
     for(let i =0; i < carrito.length ; i++){
@@ -41,11 +78,12 @@ function addItemCarrito(newItem){
             return null;
         }
     }
-    
+
     carrito.push(newItem)
     renderCarrito()
-}
-
+   
+  }
+ 
 function renderCarrito(){
     tbody.innerHTML = ""
     carrito.map(item => {
@@ -61,7 +99,7 @@ function renderCarrito(){
         <td class="table__precio"><p>${item.precio}</p></td>
         <td class="table__cantidad">
           <input type="number" min="1" value=${item.cantidad} class="input__elemento">
-          <button class="delete btn btn-danger">X</button>
+          <button class="btn btn-danger delete">X</button>
         </td>
         
         `
@@ -70,10 +108,12 @@ function renderCarrito(){
 
         tr.querySelector(".delete").addEventListener("click", removeItemCarrito)
         tr.querySelector(".input__elemento").addEventListener("change", sumaCantidad)
+
     })
+    
     CarritoTotal()
 }
-
+// suma los productos del carrito y nos da el total
 function CarritoTotal(){
     let Total = 0;
     const itemCartTotal = document.querySelector(".itemCartTotal")
@@ -85,7 +125,7 @@ function CarritoTotal(){
     itemCartTotal.innerHTML = `Total $ ${Total}`
     addLocalStorage()
 }
-
+//remover productos del carrito
 function removeItemCarrito(e){
     const buttonDelete = e.target
     const tr = buttonDelete.closest(".ItemCarrito")
@@ -95,18 +135,49 @@ function removeItemCarrito(e){
             carrito.splice(i, 1)
         }
     }
+    //alert de eliminar producto
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Estas seguro?',
+        text: "Se eliminará el Producto del carrito",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, Borrar!',
+        cancelButtonText: 'No, cancelar!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Eliminado!',
+            'Tu producto ha sido removido del carrito.',
+            'success'
+            )
 
-    const alert = document.querySelector(".remove")
+            tr.remove()
+            CarritoTotal()
 
-    setTimeout(function(){
-        alert.classList.add("remove")
-    }, 2000)
-        alert.classList.remove("remove")
+        } else if (
 
-    tr.remove()
-    CarritoTotal()
-}
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelado',
+            'Tu Producto sigue en el carrito',
+            'error'            
+          )
+        }
+      })
 
+    }
+
+//suma la cantidad de un mismo producto para que no se repita en el carrito 
 function sumaCantidad(e){
     const sumaInput = e.target
     const tr = sumaInput.closest(".ItemCarrito")
@@ -119,7 +190,7 @@ function sumaCantidad(e){
         }
     })
 }
-
+//localstorage
 function addLocalStorage(){
     localStorage.setItem("carrito", JSON.stringify(carrito))
 }
@@ -131,3 +202,29 @@ window.onload = function(){
         renderCarrito()
     }
 }
+//alert de agregar productos
+function modal (imgurl){  
+  Swal.fire({
+    title: 'Se agregó tu Producto al carrito',
+    text: 'Muchas Gracias!.',
+    imageUrl: imgurl,
+    imageWidth: 500,
+    imageHeight: 500,
+    imageAlt: 'Custom image',
+})
+}
+ //alert boton de comprar 
+const comprarCarrito = document.getElementById("btncomprar")
+
+comprarCarrito.addEventListener("click", function(){
+  Swal.fire({
+    title: 'Muchas Gracias por su Compra',
+    width: 600,
+    padding: '3em',
+    color: '#716add',
+    background: `#fff`,
+    backdrop: `
+      rgba(0,0,123,0.4)
+    `
+  })
+})
